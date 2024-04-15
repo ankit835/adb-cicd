29243b8d64eb7cc15d2dec62da13e5efb7e40f2e
ankit835 <38327231+ankit835@users.noreply.github.com>
2024-04-15 22:28:50 +0530

    Update nb1.py

--- DDL/nb1.py
+++ DDL/nb1.py
@@ -3,16 +3,5 @@
 # MAGIC DROP TABLE IF EXISTS stock_list;
 # MAGIC CREATE TABLE stock_list using CSV options (path "/FileStore/tables/sprice.csv", header "true")
 
-# COMMAND ----------
 
-# MAGIC %sql
-# MAGIC select * from stock_list
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from stock_list
-
-# MAGIC %sql
-# MAGIC select * from stock_list
 
